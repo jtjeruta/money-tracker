003dde14098592c5321fcb6e9728f72fde957275
tristan <jt.jeruta@gmail.com>
2023-04-09 20:14:25 +0800

    Update colors

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,7 +12,7 @@ export interface CardProps {
 const Card = (props: CardProps) => {
   const { className, children, title } = props;
 
-  const classes = classNames('bg-blue-100 dark:bg-slate-800 shadow-md rounded-md p-3', className);
+  const classes = classNames('bg-blue-100 dark:bg-zinc-800 shadow-md rounded-md p-3', className);
 
   return (
     <div className={classes} onClick={props.onClick}>
